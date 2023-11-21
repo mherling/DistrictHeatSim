6b437c7b309e4a957ed552560cf76a03df55ad2c
Jonas Pfeiffer <128364930+JonasPfeiffer123@users.noreply.github.com>
2023-11-21 11:09:15 +0100

    Add files via upload

--- net_generation_qgis_ETRS89_MST.py
+++ net_generation_qgis_ETRS89_MST.py
@@ -8,19 +8,22 @@ layer_points = QgsProject.instance().mapLayersByName('Beispieldaten_ETRS89')[0]
 layer_lines = QgsProject.instance().mapLayersByName('Straßen Zittau')[0]
 layer_WEA = QgsProject.instance().mapLayersByName('Erzeugerstandorte')[0]
 
-def create_layer(layer_name, layer_type, crs):
-    vl = QgsVectorLayer(f"{layer_type}?crs={crs}", layer_name, "memory")
-    provider = vl.dataProvider()
+
+def create_layer(layer_name, layer_type, crs_i):
+    vl_i = QgsVectorLayer(f"{layer_type}?crs={crs_i}", layer_name, "memory")
+    provider = vl_i.dataProvider()
     provider.addAttributes([QgsField("id", QVariant.Int)])
-    vl.updateFields()
-    vl.startEditing()
-    return vl, provider
+    vl_i.updateFields()
+    vl_i.startEditing()
+    return vl_i, provider
+
 
 def create_offset_points(point, distance, angle_degrees):
     angle_radians = math.radians(angle_degrees)
     dx = distance * math.cos(angle_radians)
     dy = distance * math.sin(angle_radians)
     return QgsPointXY(point.x() + dx, point.y() + dy)
+
 
 def generate_lines(layer, distance, angle_degrees, provider):
     for point_feat in layer.getFeatures():
@@ -33,6 +36,7 @@ def generate_lines(layer, distance, angle_degrees, provider):
         new_line.setGeometry(line)
         provider.addFeatures([new_line])
 
+
 def find_nearest_point(current_point, other_points):
     min_distance = float('inf')
     nearest_point = None
@@ -44,7 +48,8 @@ def find_nearest_point(current_point, other_points):
                 min_distance = distance
                 nearest_point = other_point
     return nearest_point
-    
+
+
 def find_nearest_line(point_geom, line_layer):
     min_distance = float('inf')
     nearest_line_feat = None
@@ -56,6 +61,7 @@ def find_nearest_line(point_geom, line_layer):
             min_distance = distance
             nearest_line_feat = line_feat
     return nearest_line_feat
+
 
 def create_perpendicular_line(point_geom, line_geom, provider):
     closest_point = line_geom.closestSegmentWithContext(point_geom.asPoint())[1]
@@ -69,6 +75,7 @@ def create_perpendicular_line(point_geom, line_geom, provider):
     
     return QgsPointXY(closest_point)
 
+
 def process_layer_points(layer, provider):
     street_end_points = set()
     for point_feat in layer.getFeatures():
@@ -80,6 +87,7 @@ def process_layer_points(layer, provider):
             street_end_point = create_perpendicular_line(point_geom, nearest_line_geom, provider)
             street_end_points.add(street_end_point)
     return street_end_points
+
 
 # generate the offset points and perpendicular lines for return lines
 def generate_return_lines(layer, distance, angle_degrees, provider):
@@ -98,19 +106,20 @@ def generate_return_lines(layer, distance, angle_degrees, provider):
             street_end_points.add(street_end_point)
     return street_end_points
 
+
 # network generation with MST (Minimum Spanning Tree)
-def generate_MST(all_end_points, provider):
+def generate_mst(all_end_points, provider):
     # create the MST-graph
-    G = nx.Graph()
+    g = nx.Graph()
 
     for point in all_end_points:
         for other_point in all_end_points:
             if point != other_point:
                 distance = point.distance(other_point)
-                G.add_edge((point.x(), point.y()), (other_point.x(), other_point.y()), weight=distance)
+                g.add_edge((point.x(), point.y()), (other_point.x(), other_point.y()), weight=distance)
     
-    #calculate the MST
-    mst = nx.minimum_spanning_tree(G)
+    # calculate the MST
+    mst = nx.minimum_spanning_tree(g)
     
     # adding the MST to the layer
     for edge in mst.edges(data=True):
@@ -124,34 +133,37 @@ def generate_MST(all_end_points, provider):
         line_feature.setAttributes([distance])
         provider.addFeatures([line_feature])
 
+
 # generate network for forward lines
-def generate_Network_fl(layer_points, layer_WEA, provider):
+def generate_network_fl(layer_points_fl, layer_wea, provider):
     # Verwenden Sie die Funktion für beide Layer
-    points_end_points = process_layer_points(layer_points, provider)
-    wea_end_points = process_layer_points(layer_WEA, provider)
+    points_end_points = process_layer_points(layer_points_fl, provider)
+    wea_end_points = process_layer_points(layer_wea, provider)
     
     # Vereinigen Sie die Endpunkte und sortieren Sie sie
     all_end_points = sorted(points_end_points.union(wea_end_points), key=lambda p: (p.x(), p.y()))
     
-    generate_MST(all_end_points, provider) 
+    generate_mst(all_end_points, provider)
+
 
 # generate network for return lines
-def generate_Network_rl(layer_points, layer_WEA, fixed_distance, fixed_angle, provider):
+def generate_network_rl(layer_points_rl, layer_wea, fixed_distance_rl, fixed_angle_rl, provider):
     # Verwenden Sie die Funktion für beide Layer
     # Generate return lines for both layers
-    points_end_points = generate_return_lines(layer_points, fixed_distance, fixed_angle, provider)
-    points_end_WEA = generate_return_lines(layer_WEA, fixed_distance, fixed_angle, provider)
+    points_end_points = generate_return_lines(layer_points_rl, fixed_distance_rl, fixed_angle_rl, provider)
+    points_end_wea = generate_return_lines(layer_wea, fixed_distance_rl, fixed_angle_rl, provider)
     
     # Vereinigen Sie die Endpunkte und sortieren Sie sie
-    all_end_points = sorted(points_end_points.union(points_end_WEA), key=lambda p: (p.x(), p.y()))
+    all_end_points = sorted(points_end_points.union(points_end_wea), key=lambda p: (p.x(), p.y()))
     
-    generate_MST(all_end_points, provider)
-    
+    generate_mst(all_end_points, provider)
+
+
 # create the layers
 crs = layer_points.crs().toWkt()
 vl_hast, provider_hast = create_layer("HAST", "Linestring", crs)
-vl_ruecklauf, provider_ruecklauf = create_layer("Rücklauf", "Linestring", crs)
-vl_vorlauf, provider_vorlauf = create_layer("Vorlauf", "Linestring", crs)
+vl_rl, provider_rl = create_layer("Rücklauf", "Linestring", crs)
+vl_vl, provider_vl = create_layer("Vorlauf", "Linestring", crs)
 vl_erzeugeranlagen, provider_erzeugeranlagen = create_layer("Erzeugeranlagen", "Linestring", crs)
 
 # Set your desired offset distance between forward and return lines here
@@ -165,8 +177,8 @@ generate_lines(layer_points, fixed_distance, fixed_angle, provider_hast)
 generate_lines(layer_WEA, fixed_distance, fixed_angle, provider_erzeugeranlagen)
 
 # generate network - fl stands for forward lines, rl for return lines
-generate_Network_fl(layer_points, layer_WEA, provider_vorlauf)
-generate_Network_rl(layer_points, layer_WEA, fixed_distance, fixed_angle, provider_ruecklauf)
+generate_network_fl(layer_points, layer_WEA, provider_vl)
+generate_network_rl(layer_points, layer_WEA, fixed_distance, fixed_angle, provider_rl)
 
 
 # commit changes for all layers
@@ -183,13 +195,13 @@ for vl, color, output_file in zip([vl_hast, vl_ruecklauf, vl_vorlauf, vl_erzeuge
     error = QgsVectorFileWriter.writeAsVectorFormat(vl, output_file, "utf-8", vl.crs(), "GeoJSON")
     if error[0] == QgsVectorFileWriter.NoError:
         print(f"Schreiben des Layers {vl.name()} als GeoJSON war erfolgreich!")
-        vlayer = QgsVectorLayer(output_file, vl.name(), "ogr")
+        v_layer = QgsVectorLayer(output_file, vl.name(), "ogr")
         
         # coloring the different layers
         symbol = QgsLineSymbol.createSimple({'line_color': color, 'line_width': '0.75'})
         renderer = QgsSingleSymbolRenderer(symbol)
-        vlayer.setRenderer(renderer)
-        if not vlayer.isValid():
+        v_layer.setRenderer(renderer)
+        if not v_layer.isValid():
             print(f"Layer {vl.name()} konnte nicht geladen werden!")
         else:
             QgsProject.instance().addMapLayer(vlayer)
---
+++ net_simulation_pandapipes.py
@@ -0,0 +1,151 @@
+import pandapipes as pp
+import pandapipes.plotting as plot
+import geopandas as gpd
+
+
+# Zugriff auf die Koordinaten jeder Linie
+def get_line_coords_and_lengths(gdf):
+    all_line_coords, all_line_lengths = [], []
+    # Berechnung der Länge jeder Linie
+    gdf['length'] = gdf.geometry.length
+    for index, row in gdf.iterrows():
+        line = row['geometry']
+        
+        # Überprüfen, ob die Geometrie ein LineString ist
+        if line.geom_type == 'LineString':
+            # Zugriff auf die Koordinatenpunkte
+            coords = list(line.coords)
+            length = row['length']
+            all_line_coords.append(coords)
+            all_line_lengths.append(length)
+        else:
+            print(f"Geometrie ist kein LineString: {line.type}")
+
+    return all_line_coords, all_line_lengths
+
+
+def get_all_point_coords_from_line_cords(all_line_coords):
+    point_coords = [koordinate for paar in all_line_coords for koordinate in paar]
+    # Entfernen von Duplikaten
+    unique_point_coords = list(set(point_coords))
+    return unique_point_coords
+
+
+def create_network(gdf_vorlauf, gdf_rücklauf, gdf_HAST, gdf_WEA):
+    def create_junctions_from_coords(net, all_coords):
+        junction_dict = {}
+        for i, coords in enumerate(all_coords, start=0):
+            junction_id = pp.create_junction(net, pn_bar=1.05, tfluid_k=293.15, name=f"Junction {i}", geodata=coords)
+            junction_dict[coords] = junction_id
+        return junction_dict
+
+    def create_pipes(net, all_line_coords, all_line_lengths, junction_dict, pipe_type):
+        for coords, length, i in zip(all_line_coords, all_line_lengths, range(0, len(all_line_coords))):
+            pp.create_pipe_from_parameters(
+                net, from_junction=junction_dict[coords[0]], to_junction=junction_dict[coords[1]],
+                length_km=length / 1000, diameter_m=0.05, k_mm=.1, alpha_w_per_m2k=10, name=f"{pipe_type} Pipe {i}",
+                sections=5, text_k=283)
+
+    def create_heat_exchangers(net, all_coords, q_heat_exchanger, junction_dict, name_prefix):
+        for i, coords in enumerate(all_coords, start=0):
+            pp.create_heat_exchanger(net, from_junction=junction_dict[coords[0]], to_junction=junction_dict[coords[1]],
+                                     diameter_m=0.02, loss_coefficient=100, qext_w=q_heat_exchanger,
+                                     name=f"{name_prefix} {i}")
+
+    def create_circulation_pump_pressure(net, all_coords, junction_dict, name_prefix):
+        for i, coords in enumerate(all_coords, start=0):
+            pp.create_circ_pump_const_pressure(net, junction_dict[coords[1]], junction_dict[coords[0]],
+                                               p_flow_bar=4, plift_bar=1.5, t_flow_k=273.15 + 90, type="auto",
+                                               name=f"{name_prefix} {i}")
+
+    net = pp.create_empty_network(fluid="water")
+
+    # Verarbeiten von Vorlauf und Rücklauf
+    junction_dict_vl = create_junctions_from_coords(net, get_all_point_coords_from_line_cords(
+        get_line_coords_and_lengths(gdf_vorlauf)[0]))
+    junction_dict_rl = create_junctions_from_coords(net, get_all_point_coords_from_line_cords(
+        get_line_coords_and_lengths(gdf_rücklauf)[0]))
+
+    # Erstellen der Pipes
+    create_pipes(net, *get_line_coords_and_lengths(gdf_vorlauf), junction_dict_vl, "Vorlauf")
+    create_pipes(net, *get_line_coords_and_lengths(gdf_rücklauf), junction_dict_rl, "Rücklauf")
+
+    # Erstellen der Heat Exchangers
+    create_heat_exchangers(net, get_line_coords_and_lengths(gdf_HAST)[0], 60000,
+                           {**junction_dict_vl, **junction_dict_rl}, "HAST")
+
+    # Erstellen der circulation pump pressure
+    create_circulation_pump_pressure(net, get_line_coords_and_lengths(gdf_WEA)[0], {**junction_dict_vl,
+                                                                                    **junction_dict_rl}, "WEA")
+    return net
+
+def correct_flow_directions(net):
+    # initiale Pipeflow-Berechnung
+    pp.pipeflow(net, mode="all")
+
+    # Überprüfen Sie die Geschwindigkeiten in jeder Pipe und tauschen Sie die Junctions bei Bedarf
+    for pipe_idx in net.pipe.index:
+        # Überprüfen Sie die mittlere Geschwindigkeit in der Pipe
+        if net.res_pipe.v_mean_m_per_s[pipe_idx] < 0:
+            # Tauschen Sie die Junctions
+            from_junction = net.pipe.at[pipe_idx, 'from_junction']
+            to_junction = net.pipe.at[pipe_idx, 'to_junction']
+            net.pipe.at[pipe_idx, 'from_junction'] = to_junction
+            net.pipe.at[pipe_idx, 'to_junction'] = from_junction
+
+    # Führen Sie die Pipeflow-Berechnung erneut durch, um aktualisierte Ergebnisse zu erhalten
+    pp.pipeflow(net, mode="all")
+
+    return net
+
+def optimize_diameter(initial_net, v_max=1, v_min=0.8, dx=0.001):
+    pp.pipeflow(initial_net, mode="all")
+    velocities = list(initial_net.res_pipe.v_mean_m_per_s)
+
+    while max(velocities) > v_max or min(velocities) < v_min:
+        for pipe_idx in initial_net.pipe.index:
+            # Überprüfen Sie die mittlere Geschwindigkeit in der Pipe
+            if initial_net.res_pipe.v_mean_m_per_s[pipe_idx] > v_max:
+                # Durchmesser vergrößern
+                initial_net.pipe.at[pipe_idx, 'diameter_m'] = initial_net.pipe.at[pipe_idx, 'diameter_m'] + dx
+            elif initial_net.res_pipe.v_mean_m_per_s[pipe_idx] < v_min:
+                # Durchmesser verkleinern
+                initial_net.pipe.at[pipe_idx, 'diameter_m'] = initial_net.pipe.at[pipe_idx, 'diameter_m'] - dx
+        pp.pipeflow(initial_net, mode="all")
+        velocities = list(initial_net.res_pipe.v_mean_m_per_s)
+
+        print(velocities)
+
+    return initial_net
+
+# GeoJSON-Datei einlesen
+gdf_vorlauf = gpd.read_file('Vorlauf.geojson')
+gdf_rücklauf = gpd.read_file('Rücklauf.geojson')
+gdf_HAST = gpd.read_file('HAST.geojson')
+gdf_WEA = gpd.read_file('Erzeugeranlagen.geojson')
+
+print("im here")
+net = create_network(gdf_vorlauf,gdf_rücklauf,gdf_HAST,gdf_WEA)
+print("im here")
+net = correct_flow_directions(net)
+print("im here")
+print(net.res_pipe)
+
+net = optimize_diameter(net)
+print("im here")
+
+#print(net.junction)
+#print(net.junction_geodata)
+#print(net.pipe)
+#print(net.heat_exchanger)
+#print(net.circ_pump_pressure)
+
+#print(net.res_junction)
+print(net.res_pipe)
+print(net.res_heat_exchanger)
+#print(net.res_circ_pump_pressure)
+
+plot.simple_plot(net, junction_size=0.2, heat_exchanger_size=0.2, pump_size=0.2, pump_color='green',
+                 pipe_color='black', heat_exchanger_color='blue')
+
+
