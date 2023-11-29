b7a9ea1d689c03ebf69f706dfa1405ce23666441
Jonas Pfeiffer <128364930+JonasPfeiffer123@users.noreply.github.com>
2023-11-29 17:26:14 +0100

    Add files via upload

--- net_simulation.py
+++ net_simulation.py
@@ -1,37 +1,143 @@
 import pandapipes as pp
 import pandapipes.plotting as plot
+from pandapipes.timeseries import run_time_series
+from pandapower.control.controller.const_control import ConstControl
+from pandapower.timeseries import OutputWriter
+from pandapower.timeseries import DFData
+
 import geopandas as gpd
+import pandas as pd
+import matplotlib.pyplot as plt
+
 import net_simulation_pandapipes as nsp
 
-# GeoJSON-Dateien einlesen
-gdf_vl = gpd.read_file('geoJSON_Vorlauf.geojson')
-gdf_rl = gpd.read_file('geoJSON_Rücklauf.geojson')
-gdf_HAST = gpd.read_file('geoJSON_HAST.geojson')
-gdf_WEA = gpd.read_file('geoJSON_Erzeugeranlagen.geojson')
+def initialize_test_net():
+    net = pp.create_empty_network(fluid="water")
+
+    # Junctions for pump
+    j1 = pp.create_junction(net, pn_bar=1.05, tfluid_k=293.15, name="Junction 1")
+    j2 = pp.create_junction(net, pn_bar=1.05, tfluid_k=293.15, name="Junction 2")
+
+    # Junctions for connection pipes forward line
+    j3 = pp.create_junction(net, pn_bar=1.05, tfluid_k=293.15, name="Junction 3")
+    j4 = pp.create_junction(net, pn_bar=1.05, tfluid_k=293.15, name="Junction 4")
+
+    # Junctions for heat exchangers
+    j5 = pp.create_junction(net, pn_bar=1.05, tfluid_k=293.15, name="Junction 5")
+    j6 = pp.create_junction(net, pn_bar=1.05, tfluid_k=293.15, name="Junction 6")
+    
+    # Junctions for connection pipes 
+    j7 = pp.create_junction(net, pn_bar=1.05, tfluid_k=293.15, name="Junction 7")
+    j8 = pp.create_junction(net, pn_bar=1.05, tfluid_k=293.15, name="Junction 8")
+
+    pump1 = pp.create_circ_pump_const_pressure(net, j1, j2, p_flow_bar=4,
+                                               plift_bar=1.5, t_flow_k=273.15 + 90,
+                                               type="auto", name="pump1")
+
+    pipe1 = pp.create_pipe(net, j2, j3, std_type="110_PE_100_SDR_17", length_km=0.01,
+                           k_mm=.1, alpha_w_per_m2k=10, name="pipe1", sections=5,
+                           text_k=283)
+    pipe2 = pp.create_pipe(net, j3, j4, std_type="110_PE_100_SDR_17", length_km=0.5,
+                           k_mm=.1, alpha_w_per_m2k=10, name="pipe2", sections=5,
+                           text_k=283)
+    pipe3 = pp.create_pipe(net, j4, j5, std_type="110_PE_100_SDR_17", length_km=0.01,
+                           k_mm=.1, alpha_w_per_m2k=10, name="pipe3", sections=5,
+                           text_k=283)
+    
+    heat_exchanger1 = pp.create_heat_exchanger(net, j5, j6, diameter_m=0.02,
+                                               loss_coefficient=100, qext_w=50000,
+                                               name="heat_exchanger1")
+
+    pipe4 = pp.create_pipe(net, j6, j7, std_type="110_PE_100_SDR_17", length_km=0.01,
+                           k_mm=.1, alpha_w_per_m2k=10, name="pipe4", sections=5,
+                           text_k=283)
+    pipe5 = pp.create_pipe(net, j7, j8, std_type="110_PE_100_SDR_17", length_km=0.5,
+                           k_mm=.1, alpha_w_per_m2k=10, name="pipe5", sections=5,
+                           text_k=283)
+    pipe6 = pp.create_pipe(net, j8, j1, std_type="110_PE_100_SDR_17", length_km=0.01,
+                           k_mm=.1, alpha_w_per_m2k=10, name="pipe6", sections=5,
+                           text_k=283)
+
+    pp.pipeflow(net, mode="all")
+    return net
+
+def initialize_net():
+    # GeoJSON-Dateien einlesen
+    gdf_vl = gpd.read_file('geoJSON_Vorlauf.geojson')
+    gdf_rl = gpd.read_file('geoJSON_Rücklauf.geojson')
+    gdf_HAST = gpd.read_file('geoJSON_HAST.geojson')
+    gdf_WEA = gpd.read_file('geoJSON_Erzeugeranlagen.geojson')
+
+    pipe_creation_mode = "type"
+    # pipe_creation_mode = "diameter"
+
+    net = nsp.create_network(gdf_vl, gdf_rl, gdf_HAST, gdf_WEA, pipe_creation_mode)
+    net = nsp.correct_flow_directions(net)
+
+    if pipe_creation_mode == "diameter":
+        net = nsp.optimize_diameter_parameters(net)
+
+    if pipe_creation_mode == "type":
+        net = nsp.optimize_diameter_types(net)
+
+    #plot.simple_plot(net, junction_size=0.2, heat_exchanger_size=0.2, pump_size=0.2, pump_color='green',
+     #                pipe_color='black', heat_exchanger_color='blue')
+
+    nsp.export_net_geojson(net)
+
+    # print(net.junction)
+    # print(net.junction_geodata)
+    # print(net.pipe)
+    # print(net.heat_exchanger)
+    # print(net.circ_pump_pressure)
+
+    # print(net.res_junction)
+    # print(net.res_pipe)
+    # print(net.res_heat_exchanger)
+    # print(net.res_circ_pump_pressure)
+
+    return(net)
+
+def time_series_net(net):
+    time_steps = range(0, 24)  # hourly time steps
+    df = pd.DataFrame(index=time_steps)
+
+    qext_w_profile = [50000] * 3 + [60000] * 6 + [70000] * 9 + [80000] * 6
+    
+    for i in range(1):
+
+        df = pd.DataFrame(index=time_steps, data={'qext_w_'+str(i): qext_w_profile})
+
+        data_source = DFData(df)
+        ConstControl(net, element='heat_exchanger', variable='qext_w', element_index=[i], data_source=data_source, profile_name='qext_w_'+str(i))
+
+    log_variables = [('res_junction', 'p_bar'), ('res_pipe', 'v_mean_m_per_s'),
+                     ('res_pipe', 'reynolds'), ('res_pipe', 'lambda'), ('heat_exchanger', 'qext_w'),
+                     ('res_heat_exchanger', 'v_mean_m_per_s'), ('res_heat_exchanger', 't_from_k'),
+                     ('res_heat_exchanger', 't_to_k'), ('circ_pump_pressure', 't_flow_k'), ('res_junction', 't_k')]
+    
+    ow = OutputWriter(net, time_steps, output_path=None, log_variables=log_variables)
+
+    run_time_series.run_timeseries(net, time_steps, mode="all")
+
+    print("temperature:")
+    print(ow.np_results["res_junction.t_k"])
+
+    x = time_steps
+    y1 = ow.np_results["res_heat_exchanger.t_from_k"]
+    y2 = ow.np_results["res_heat_exchanger.t_to_k"]
+    
+    plt.xlabel("time step")
+    plt.ylabel("temperature [K]")
+    plt.title("temperature profile heat exchangers")
+    plt.plot(x, y1[:,0], "g-o")
+    plt.plot(x, y2[:,0], "b-o")
+    plt.legend(["heat exchanger 1 from", "heat exchanger 1 to"], loc='lower left')
+    plt.grid()
+    plt.show()
 
 
-net = nsp.create_network(gdf_vl, gdf_rl, gdf_HAST, gdf_WEA)
-
-net = nsp.correct_flow_directions(net)
-
-net = nsp.optimize_diameter_parameters(net)
-
-# Auflisten aller verfügbaren Standardtypen für Rohre
-pipe_std_types = pp.std_types.available_std_types(net, "pipe")
-# print(pipe_std_types)
-
-# print(net.junction)
-# print(net.junction_geodata)
-# print(net.pipe)
-# print(net.heat_exchanger)
-# print(net.circ_pump_pressure)
-
-# print(net.res_junction)
-# print(net.res_pipe)
-# print(net.res_heat_exchanger)
-# print(net.res_circ_pump_pressure)
-
-plot.simple_plot(net, junction_size=0.2, heat_exchanger_size=0.2, pump_size=0.2, pump_color='green',
-                 pipe_color='black', heat_exchanger_color='blue')
-
-nsp.export_net_geojson(net)
+net = initialize_net()
+# net = initialize_test_net()
+print(net.res_junction)
+time_series_net(net)
--- net_simulation_pandapipes.py
+++ net_simulation_pandapipes.py
@@ -1,5 +1,6 @@
 import pandapipes as pp
-
+import geopandas as gpd
+from shapely.geometry import LineString
 
 # Zugriff auf die Koordinaten jeder Linie
 def get_line_coords_and_lengths(gdf):
@@ -29,7 +30,7 @@ def get_all_point_coords_from_line_cords(all_line_coords):
     return unique_point_coords
 
 
-def create_network(gdf_vorlauf, gdf_rl, gdf_hast, gdf_wea):
+def create_network(gdf_vorlauf, gdf_rl, gdf_hast, gdf_wea, pipe_creation_mode="diameter"):
     def create_junctions_from_coords(net_i, all_coords):
         junction_dict = {}
         for i, coords in enumerate(all_coords, start=0):
@@ -37,12 +38,18 @@ def create_network(gdf_vorlauf, gdf_rl, gdf_hast, gdf_wea):
             junction_dict[coords] = junction_id
         return junction_dict
 
-    def create_pipes(net_i, all_line_coords, all_line_lengths, junction_dict, pipe_type):
-        for coords, length, i in zip(all_line_coords, all_line_lengths, range(0, len(all_line_coords))):
-            pp.create_pipe_from_parameters(
-                net_i, from_junction=junction_dict[coords[0]], to_junction=junction_dict[coords[1]],
-                length_km=length / 1000, diameter_m=0.05, k_mm=.1, alpha_w_per_m2k=10, name=f"{pipe_type} Pipe {i}",
-                sections=5, text_k=283)
+    def create_pipes_diameter(net_i, all_line_coords, all_line_lengths, junction_dict, pipe_type, diameter_mm):
+        for coords, length_m, i in zip(all_line_coords, all_line_lengths, range(0, len(all_line_coords))):
+            pp.create_pipe_from_parameters(net_i, from_junction=junction_dict[coords[0]],
+                                           to_junction=junction_dict[coords[1]], length_km=length_m/1000,
+                                           diameter_m=diameter_mm/1000, k_mm=.1, alpha_w_per_m2k=10, name=f"{pipe_type} Pipe {i}",
+                                           geodata=coords, sections=5, text_k=283)
+
+    def create_pipes_type(net_i, all_line_coords, all_line_lengths, junction_dict, line_type, pipetype):
+        for coords, length_m, i in zip(all_line_coords, all_line_lengths, range(0, len(all_line_coords))):
+            pp.create_pipe(net_i, from_junction=junction_dict[coords[0]], to_junction=junction_dict[coords[1]],
+                           std_type=pipetype, length_km=length_m/1000, k_mm=.1, alpha_w_per_m2k=10,
+                           name=f"{line_type} Pipe {i}", geodata=coords, sections=5, text_k=283)
 
     def create_heat_exchangers(net_i, all_coords, q_heat_exchanger, junction_dict, name_prefix):
         for i, coords in enumerate(all_coords, start=0):
@@ -65,8 +72,15 @@ def create_network(gdf_vorlauf, gdf_rl, gdf_hast, gdf_wea):
         get_line_coords_and_lengths(gdf_rl)[0]))
 
     # Erstellen der Pipes
-    create_pipes(net, *get_line_coords_and_lengths(gdf_vorlauf), junction_dict_vl, "Vorlauf")
-    create_pipes(net, *get_line_coords_and_lengths(gdf_rl), junction_dict_rl, "Rücklauf")
+    if pipe_creation_mode == "diameter":
+        diameter_mm = 100
+        create_pipes_diameter(net, *get_line_coords_and_lengths(gdf_vorlauf), junction_dict_vl, "Vorlauf", diameter_mm)
+        create_pipes_diameter(net, *get_line_coords_and_lengths(gdf_rl), junction_dict_rl, "Rücklauf", diameter_mm)
+
+    if pipe_creation_mode == "type":
+        pipetype = "110_PE_100_SDR_17"
+        create_pipes_type(net, *get_line_coords_and_lengths(gdf_vorlauf), junction_dict_vl, "Vorlauf", pipetype)
+        create_pipes_type(net, *get_line_coords_and_lengths(gdf_rl), junction_dict_rl, "Rücklauf", pipetype)
 
     # Erstellen der Heat Exchangers
     create_heat_exchangers(net, get_line_coords_and_lengths(gdf_hast)[0], 60000,
@@ -101,7 +115,7 @@ def correct_flow_directions(net):
 def optimize_diameter_parameters(initial_net, v_max=1, v_min=0.8, dx=0.001):
     pp.pipeflow(initial_net, mode="all")
     velocities = list(initial_net.res_pipe.v_mean_m_per_s)
-
+    
     while max(velocities) > v_max or min(velocities) < v_min:
         for pipe_idx in initial_net.pipe.index:
             # Überprüfen Sie die mittlere Geschwindigkeit in der Pipe
@@ -116,24 +130,59 @@ def optimize_diameter_parameters(initial_net, v_max=1, v_min=0.8, dx=0.001):
 
     return initial_net
 
+def optimize_diameter_types(net, v_max=1.1, v_min=0.7):
+    pp.pipeflow(net, mode="all")
+    velocities = list(net.res_pipe.v_mean_m_per_s)
 
-def optimize_diameter_types(initial_net, v_max=1, v_min=0.8):
-    pp.pipeflow(initial_net, mode="all")
-    velocities = list(initial_net.res_pipe.v_mean_m_per_s)
+    # Auflisten aller verfügbaren Standardtypen für Rohre
+    pipe_std_types = pp.std_types.available_std_types(net, "pipe")
+    # Filtern nach einem bestimmten Material, z.B. "PE 100"
+    filtered_pipe_types = pipe_std_types[pipe_std_types['material'] == 'PE 100']
 
-    while max(velocities) > v_max or min(velocities) < v_min:
-        for pipe_idx in initial_net.pipe.index:
-            # Überprüfen Sie die mittlere Geschwindigkeit in der Pipe
-            if initial_net.res_pipe.v_mean_m_per_s[pipe_idx] > v_max:
-                # Durchmesser vergrößern
-                initial_net.pipe.at[pipe_idx, 'std_type'] = -1  # kleinerer Typ
-            elif initial_net.res_pipe.v_mean_m_per_s[pipe_idx] < v_min:
-                # Durchmesser verkleinern
-                initial_net.pipe.at[pipe_idx, 'std_type'] = +1  # größerer Typ
-        pp.pipeflow(initial_net, mode="all")
-        velocities = list(initial_net.res_pipe.v_mean_m_per_s)
+    # Erstellen eines Dictionarys, das die Position jedes Rohrtyps im gefilterten DataFrame enthält
+    type_position_dict = {type_name: i for i, type_name in enumerate(filtered_pipe_types.index)}
+    
+    while any(v > v_max or v < v_min for v in net.res_pipe.v_mean_m_per_s):
+        for pipe_idx, velocity in enumerate(net.res_pipe.v_mean_m_per_s):
+            current_type = net.pipe.std_type.at[pipe_idx]
+            current_type_position = type_position_dict[current_type]
 
-    return initial_net
+            if velocity > v_max and current_type_position > 0:
+                 # Aktualisieren Sie den Rohrtyp auf den vorherigen Typ
+                new_type = filtered_pipe_types.index[current_type_position + 1]
+                net.pipe.std_type.at[pipe_idx] = new_type
+                # Aktualisieren Sie die Eigenschaften des Rohres
+                properties = filtered_pipe_types.loc[new_type]
+                net.pipe.at[pipe_idx, 'diameter_m'] = properties['inner_diameter_mm'] / 1000
+
+            elif velocity < v_min and current_type_position < len(filtered_pipe_types) - 1:
+                # Aktualisieren Sie den Rohrtyp auf den nächsten Typ
+                new_type = filtered_pipe_types.index[current_type_position - 1]
+                net.pipe.std_type.at[pipe_idx] = new_type
+                # Aktualisieren Sie die Eigenschaften des Rohres
+                properties = filtered_pipe_types.loc[new_type]
+                net.pipe.at[pipe_idx, 'diameter_m'] = properties['inner_diameter_mm'] / 1000
+                
+        pp.pipeflow(net, mode="all")
+        velocities = list(net.res_pipe.v_mean_m_per_s)
+
+    return net
 
 def export_net_geojson(net):
-    print("Hier könnte deine geoJSON-Datei exportiert werden.")
+    # Prüfen, ob geographische Daten vorhanden sind
+    if 'pipe_geodata' in net and not net.pipe_geodata.empty:
+        # Konvertieren der Rohrdaten in ein GeoDataFrame
+        gdf = gpd.GeoDataFrame(net.pipe_geodata)
+        gdf['geometry'] = gdf['coords'].apply(lambda x: LineString(x))
+        del gdf['coords']  # Entfernen Sie die ursprüngliche Koordinatenspalte
+
+        # Hinzufügen zusätzlicher Eigenschaften wie 'diameter_m'
+        gdf['diameter_mm'] = net.pipe['diameter_m'] / 1000
+    
+        # Setzen Sie das Koordinatensystem, falls bekannt (hier beispielhaft EPSG:4326)
+        gdf.set_crs(epsg=25833, inplace=True)
+
+        # Exportieren als GeoJSON
+        gdf.to_file("pipes_network.geojson", driver='GeoJSON')
+    else:
+        print("Keine geographischen Daten im Netzwerk vorhanden.")
