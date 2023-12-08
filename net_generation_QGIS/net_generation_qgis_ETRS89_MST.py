80090e87cfa4f37a3ae55d8e8a9468a40191f6a3
Jonas Pfeiffer <jonaspfeiffer123@gmail.com>
2023-12-08 15:52:45 +0100

    updated

--- net_generation_QGIS/net_generation_qgis_ETRS89_MST.py
+++ net_generation_QGIS/net_generation_qgis_ETRS89_MST.py
@@ -85,35 +85,3 @@ def load_and_style_layer(file_path, layer_name, color):
         QgsProject.instance().addMapLayer(v_layer)
         print(f"Layer {layer_name} wurde erfolgreich geladen!")
 
-
-# Hier setzen Sie Ihre Overpass-Abfrage ein
-#overpass_query = """
-#[out:json][timeout:25];
-#area[name="Görlitz"]->.area_0;
-#(
-#  node["highway"="primary"]["highway"="secondary"]["highway"="tertiary"]["highway"="residential"]["highway"="road"]["highway"="living_street"](area.area_0);
-#  way["highway"="primary"]["highway"="secondary"]["highway"="tertiary"]["highway"="residential"]["highway"="road"]["highway"="living_street"](area.area_0);
-#  relation["highway"="primary"]["highway"="secondary"]["highway"="tertiary"]["highway"="residential"]["highway"="road"]["highway"="living_street"](area.area_0);
-#);
-#(._;>;);
-#out body;
-#"""
-
-# Ausgabedateiname für GeoJSON-Datei
-osm_street_layer_geojson_file = "C:/Users/jp66tyda/heating_network_generation/net_generation_QGIS/Beispiel Projekt/Straßen.geojson"
-
-# Download der Daten und Speichern als GeoJSON
-#download_osm_street_data(overpass_query, osm_street_layer_geojson_file)
-
-# data points csv file path
-#data_csv_file_name = "data_output_ETRS89.csv"
-data_csv_file_name = "data_output_gr_ETRS89.csv"
-
-# Koordinaten für den Erzeugerstandort
-#x_coord = 486267.306999999971595  # Longitude
-#y_coord = 5637294.910000000149012  # Latitude
-x_coord = 499827.91  # Longitude
-y_coord = 5666288.22  # Latitude
-
-layer_points, layer_lines, layer_WEA = load_layers(osm_street_layer_geojson_file, data_csv_file_name, x_coord, y_coord)
-generate_and_export_layers(layer_points, layer_lines, layer_WEA)
