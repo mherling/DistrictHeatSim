605b442f82d09ffd9be95763d494abf28a5ea9d5
Jonas Pfeiffer <jonaspfeiffer123@gmail.com>
2023-12-09 16:12:41 +0100

    updates

--- net_generation_QGIS/net_generation_qgis.py
+++ net_generation_QGIS/net_generation_qgis.py
@@ -3,11 +3,38 @@ import os
 
 from import_and_create_layers_qgis import *
 
+### Projektspezifische Eingaben ###
 #from qgis_simple_MST import *
 #from qgis_street_MST import *
 from qgis_advanced_street_MST import *
 
+# Projekt
+projekt = "Zittau"
+#projekt = "Görlitz"
 
+if projekt == "Zittau":
+    # Ausgabedateiname für GeoJSON-Datei
+    osm_street_layer_geojson_file_name = "C:/Users/jonas/heating_network_generation/net_generation_QGIS/Straßen Zittau.geojson"
+    #osm_street_layer_geojson_file_name = "C:/Users/jp66tyda/heating_network_generation/net_generation_QGIS/Straßen Zittau.geojson"
+    
+    # data points csv file path
+    data_csv_file_name = "data_output_zi_ETRS89.csv"
+    
+    # Koordinaten für den Erzeugerstandort
+    x_coord = 486267.306999999971595  # Longitude
+    y_coord = 5637294.910000000149012  # Latitude
+    
+if projekt == "Görlitz":
+    osm_street_layer_geojson_file_name = "C:/Users/jonas/heating_network_generation/net_generation_QGIS/Straßen Görlitz.geojson"
+    #osm_street_layer_geojson_file_name = "C:/Users/jp66tyda/heating_network_generation/net_generation_QGIS/Straßen Görlitz.geojson"
+    
+    # data points csv file path
+    data_csv_file_name = "data_output_gr_ETRS89.csv"
+    
+    # Koordinaten für den Erzeugerstandort
+    x_coord = 499827.91  # Longitude
+    y_coord = 5666288.22  # Latitude
+    
 def load_layers(osm_street_layer_geojson_file, data_csv_file_name, x_coord, y_coord):
     """
     Laden der erforderlichen Layer in das QGIS-Projekt.
@@ -89,33 +116,6 @@ def load_and_style_layer(file_path, layer_name, color):
     else:
         QgsProject.instance().addMapLayer(v_layer)
         print(f"Layer {layer_name} wurde erfolgreich geladen!")
-
-# Projekt
-#projekt = "Zittau"
-projekt = "Görlitz"
-
-if projekt == "Zittau":
-    # Ausgabedateiname für GeoJSON-Datei
-    osm_street_layer_geojson_file_name = "C:/Users/jonas/heating_network_generation/net_generation_QGIS/Straßen Zittau.geojson"
-    #osm_street_layer_geojson_file_name = "C:/Users/jp66tyda/heating_network_generation/net_generation_QGIS/Straßen Zittau.geojson"
-    
-    # data points csv file path
-    data_csv_file_name = "data_output_zi_ETRS89.csv"
-    
-    # Koordinaten für den Erzeugerstandort
-    x_coord = 486267.306999999971595  # Longitude
-    y_coord = 5637294.910000000149012  # Latitude
-    
-if projekt == "Görlitz":
-    osm_street_layer_geojson_file_name = "C:/Users/jonas/heating_network_generation/net_generation_QGIS/Straßen Görlitz.geojson"
-    #osm_street_layer_geojson_file_name = "C:/Users/jp66tyda/heating_network_generation/net_generation_QGIS/Straßen Görlitz.geojson"
-    
-    # data points csv file path
-    data_csv_file_name = "data_output_gr_ETRS89.csv"
-    
-    # Koordinaten für den Erzeugerstandort
-    x_coord = 499827.91  # Longitude
-    y_coord = 5666288.22  # Latitude
 
 layer_points, layer_lines, layer_WEA = load_layers(osm_street_layer_geojson_file_name, data_csv_file_name, x_coord, y_coord)
 generate_and_export_layers(layer_points, layer_lines, layer_WEA)
